c03a23865db8c733c3334a5df2411656b438c64c
Sonechka M <s212288s@gmail.com>
2020-01-08 21:39:03 -0500

    created basic structure

---
+++ script.js
@@ -0,0 +1,37 @@
+var time = 0;
+var sscore = 0;
+var startQuiz = document.querySelector("start")
+// create array that has objects as questions
+
+// on click start the game, set the time to 30, then decrement 
+startQuiz.addEventListener("click", function(){
+    event.preventDefault();
+    var time = 30;
+    // select the element where time is displayed and display it display that time
+    time.textContent = time;
+});
+
+// set timer with set interval
+// create an array of objects for questions
+
+// title, choices, answer
+
+// get rid of start quiz button when game starts 
+
+// loop through choices and add them tobuttons or paragraphs
+
+// create a function that goes through choices clicks
+
+// function to input initials
+
+// function to create end UI
+
+// start at 100 pass the game in 50 sec, get remainder seconds as your score
+
+
+
+
+
+
+
+
