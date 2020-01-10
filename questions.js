1615019be0c762228c84cdb3bb449ebb09af4538
Sonechka M <s212288s@gmail.com>
2020-01-09 23:48:13 -0500

    looping through questions, displaying correct and next

--- questions.js
+++ questions.js
@@ -8,15 +8,15 @@ var questions = [
       title: "The condition in an if / else statement is enclosed within ____.",
       choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
       answer: "parentheses"
-    },
-    {
-      title: "The weather is ____.",
-      choices: ["warm", "hot", "cold", "windy"],
-      answer: "parentheses"
-    },
-    {
-      title: "JS is ____.",
-      choices: ["hard", "easy", "cold", "windy"],
-      answer: "parentheses"
-    },
+    }
+    // {
+    //   title: "Commonly used data types DO NOT include:",
+    //   choices: ["strings", "booleans", "alerts", "numbers"],
+    //   answer: "alerts"
+    // },
+    // {
+    //   title: "The condition in an if / else statement is enclosed within ____.",
+    //   choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    //   answer: "parentheses"
+    // }
   ];
--- script.js
+++ script.js
@@ -2,8 +2,8 @@ var timer = document.querySelector("#timer");
 var startButton = document.querySelector(".start-button");
 var question = document.querySelector(".title");
 var optionsField = document.querySelector(".options-field");
-var quizTime = 3 
-var questionObjectChoices = 0
+var quizTime = 15; 
+var questionObject = 0;
 
 
 function setTimer(){
@@ -12,32 +12,69 @@ function setTimer(){
         timer.textContent = quizTime--;
 
         if (quizTime === 0) {
+            console.log("game over")
             clearInterval(timerInterval);
             timer.textContent = "0";
+            question.textContent = "";
+            optionsField.textContent = "";
+            var gameover = document.createElement("h3");
+            gameover.textContent = "Game Over!"
+            optionsField.appendChild(gameover);
         }
     }, 1000)
 }
 
 
 function displayQuestion(){
-    question.textContent = questions[questionObjectChoices].title;
+    question.textContent = questions[questionObject].title;
     displayChoices();
     }
 
 function displayChoices(){
-    for (var i = 0; i < questions[questionObjectChoices].choices.length; i++){
+    for (var i = 0; i < questions[questionObject].choices.length; i++){
         var option = document.createElement("p");
-        option.classList.add('question-option');
-        option.textContent = questions[questionObjectChoices].choices[i];
+        // option.classList.add('question-option');
+        option.textContent = questions[questionObject].choices[i];
         optionsField.appendChild(option);
+        console.log(questions)
     }
+    
 }
 
 function updateQuestion(){
-    questionObjectChoices++
-    question.textContent = questions[questionObjectChoices].title;
+    questionObject++
+    question.textContent = questions[questionObject].title;
     optionsField.textContent = ""
     displayChoices();
+}
+
+function displayNextButton(){
+    
+}
+
+function displayAnswer(){
+    if(event.toElement.innerText === questions[questionObject].answer){
+        question.textContent = "";
+        optionsField.textContent = "";
+        var correct = document.createElement("h3");
+        correct.textContent = "Correct!"
+        optionsField.appendChild(correct);
+        var next = document.createElement("p");
+        next.textContent = "next"
+        optionsField.appendChild(next);
+        next.addEventListener("click", function(){
+            updateQuestion();
+    });       
+    }
+    // if (event.toElement.innerText !== questions[questionObject].answer){
+    //     question.textContent = "";
+    //     optionsField.textContent = "";
+    //     var incorrect = document.createElement("h3");
+    //     incorrect.textContent = "Incorrect:("
+    //     optionsField.appendChild(incorrect);
+    //     // displayNextButton();
+    // }
+    
 }
 
 startButton.addEventListener ("click", function(){
@@ -45,53 +82,13 @@ startButton.addEventListener ("click", function(){
     displayQuestion();
 });
 optionsField.addEventListener("click", function(){
-    updateQuestion();
+    displayAnswer();
+    // updateQuestion(); 
 });
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// create array that has objects as questions
-
-
-
-
-
-// title, choices, answer
-
 // get rid of start quiz button when game starts 
-
-// loop through choices and add them tobuttons or paragraphs
-
-// create a function that goes through choices clicks
 
 // function to input initials
 
