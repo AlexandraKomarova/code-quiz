14f496641f08ef94e6f9d1ec1b010028a54f2adc
Sonechka M <s212288s@gmail.com>
2020-01-11 10:12:15 -0500

    now when incorrect answer is cliked incorrect message is displayed and next question is displayed in 1 second

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ var optionsField = document.querySelector(".options-field");
 var quizTime = 5000; 
 var questionObject = 0;
 
+// when start quiz is clicked set timer and display question
+startButton.addEventListener ("click", function(){
+    setTimer();
+    displayQuestion();
+});
 
 function gameOver(){
     timer.textContent = "0";
@@ -27,45 +32,29 @@ function setTimer(){
     }, 1000)
 }
 
-// need to go through questions array while 
-// var result = "";
-// do {
-//     questionObject++
-//     result = result + questionObject;
-//   } while (questionObject < 3);
-  
-//   console.log(result);
-//   // expected result: "12345"
-
 function displayQuestion(){
     question.textContent = questions[questionObject].title;
     displayChoices();
     var allChoices = document.querySelectorAll("p")
-
-    //  for(var i = 0; i < allChoices.length; i++){
-    //     allChoices[i].addEventListener("click", function(event){
-    //         console.dir(event.target.attributes.data.value)
-    //     //    displayAnswer(event)
-    //     })
-    // }
-
 }
 
 function displayChoices(){
-    // loop through choices array and place the elements as options in optionsField
-    for (var i = 0; i < questions[questionObject].choices.length; i++){
+    // var options to store arrays of choices to refer to in the loop so that the code is clearer and easier to write
+    var options = questions[questionObject].choices;
+     // loop through choices array and place the elements as options in optionsField
+    for (var i = 0; i < options.length; i++){
+        // options are created as <p> and saved in var option
         var option = document.createElement("p");
-        option.setAttribute("data", questions[questionObject].choices[i])
-        option.textContent = questions[questionObject].choices[i];
+        // to each <p> "data" attribute id added and its value is each element in choices array
+        option.setAttribute("data", options[i])
+        // update display by adding 
+        option.textContent = options[i];
         optionsField.appendChild(option);
-    
     }
-    
 }
 
 function updateQuestion(){
     questionObject++
-    console.log(questionObject)
     if (questions[questionObject]){
         question.textContent = questions[questionObject].title;
         optionsField.textContent = ""
@@ -73,81 +62,49 @@ function updateQuestion(){
     }
     else {
         gameOver()
-    }
-    
+    } 
 }
 
-function displayNextButton(){
-    
-}
-
-function displayAnswer(event){
-
-    // console.log(event.target.value)    
-
-    if(event.toElement.innerText === questions[questionObject].answer){
-        question.textContent = "";
-        optionsField.textContent = "";
-        var correct = document.createElement("h3");
-        correct.textContent = "Correct!"
-        optionsField.appendChild(correct);
-        var next = document.createElement("p");
-        next.textContent = "next"
-        optionsField.appendChild(next);
-        next.addEventListener("click", function(){
-            updateQuestion();
-    });  
-    }
-    if (event.toElement.innerText !== questions[questionObject].answer){
-        question.textContent = "";
-        optionsField.textContent = "";
-        var incorrect = document.createElement("h3");
-        incorrect.textContent = "Incorrect:("
-        optionsField.appendChild(incorrect);
-        // displayNextButton();
-        // subtract from timer when answer is wrong
-    }
-    
-}
-
-startButton.addEventListener ("click", function(){
-    setTimer();
-    displayQuestion();
-});
-
-    
+// SUBTRACT FROM TIMER WHEN ASNWER IS WRONG
 
 optionsField.addEventListener("click", function(event){
     if (event.target.matches("p")){
         var userChoice = event.target.attributes.data.value
         var answer = questions[questionObject].answer
         if(userChoice === answer){
-           // clear optiojs freld
-           question.textContent = "";
+           // clear options field
+            question.textContent = "";
             optionsField.textContent = "";
            // display correct
-           var correct = document.createElement("h3");
-        correct.textContent = "Correct!"
-        optionsField.appendChild(correct);
-        // display next question
-        setTimeout(function(){
-            if (quizTime > 0) {
-                updateQuestion()
-            }
-        }, 1000);
-        
+            var correct = document.createElement("h3");
+            correct.textContent = "Correct!"
+            optionsField.appendChild(correct);
+            // display next question in 1 second
+            setTimeout(function(){
+                if (quizTime > 0) {
+                    updateQuestion()
+                }
+            }, 1000);
         }
-        
+        else {
+            // clear options field
+            question.textContent = "";
+            optionsField.textContent = "";
+            // display incorrect
+            var incorrect = document.createElement("h3");
+            incorrect.textContent = "Incorrect:("
+            optionsField.appendChild(incorrect);
+            // display next question in 1 second
+            setTimeout(function(){
+                if (quizTime > 0) {
+                    updateQuestion()
+                }
+            }, 1000);
+        }    
     }
-    
-    
+     
 })
     
-
-
-//     displayAnswer();
-//     // updateQuestion(); 
-// });
 
 // show correct answer and then display new question
 
@@ -169,4 +126,51 @@ optionsField.addEventListener("click", function(event){
 
 
 
+// need to go through questions array while 
+// var result = "";
+// do {
+//     questionObject++
+//     result = result + questionObject;
+//   } while (questionObject < 3);
+  
+//   console.log(result);
+//   // expected result: "12345"
 
+// OLD DISPLAY ANSWER
+// function displayAnswer(event){
+
+//     // console.log(event.target.value)    
+
+//     if(event.toElement.innerText === questions[questionObject].answer){
+//         question.textContent = "";
+//         optionsField.textContent = "";
+//         var correct = document.createElement("h3");
+//         correct.textContent = "Correct!"
+//         optionsField.appendChild(correct);
+//         var next = document.createElement("p");
+//         next.textContent = "next"
+//         optionsField.appendChild(next);
+//         next.addEventListener("click", function(){
+//             updateQuestion();
+//         });  
+//     }
+//     if (event.toElement.innerText !== questions[questionObject].answer){
+//         question.textContent = "";
+//         optionsField.textContent = "";
+//         var incorrect = document.createElement("h3");
+//         incorrect.textContent = "Incorrect:("
+//         optionsField.appendChild(incorrect);
+//         // displayNextButton();
+//         // subtract from timer when answer is wrong
+//     }
+    
+// }
+
+// THIS FOR LOOP HAS CONSOLE.DIR TO BE SAVED FOR REFERENCE
+
+// //  for(var i = 0; i < allChoices.length; i++){
+//     //     allChoices[i].addEventListener("click", function(event){
+//     //         console.dir(event.target.attributes.data.value)
+//     //     //    displayAnswer(event)
+//     //     })
+//     // }
