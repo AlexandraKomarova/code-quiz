3a520a5a5032588b82ad743c0da949bd765b8a93
Sonechka M <s212288s@gmail.com>
2020-01-14 17:43:40 -0500

    view high scores prints from local storage

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var page = document.querySelector (".page")
+var time = document.querySelector(".time");
 var timer = document.querySelector("#timer");
 var startButton = document.querySelector(".start-button");
 var question = document.querySelector(".title");
@@ -11,38 +13,16 @@ var viewHighScores = document.querySelector(".view-highscore-button");
 var quizTime = 15; 
 var questionObject = 0;
 
-// ============= TO-DO LIST TO FINISH THE PROJECT =====================
-// ====================================================================
-
-// FUNCTIONALITY:
-// *************
-// HIDE START-BUTTON WHEN GAME STARTS
-// HIDE VIEW HIGHSCORES WHEN GAME STARTS
-// BRING BOTH BACK IN THE END, BUT CHANGE TEXT IN START BUTTON TO PLAY AGAIN
-
-// LOCAL STORAGE:
-// ************** 
-// GET INFO FROM LOCAL STORAGE AND USE THAT TO RENDER END SCREEN
-
-// THE LOOKS:
-// **********
-// CLEAN UP CODE
-// CHECK UX, UI, PLAY WITH THAT COMPUTER IMAGE, PROBABLY INVERT THE COLORS
-
-// when start quiz is clicked set timer and display question
-startButton.addEventListener ("click", function(){
-    setTimer();
-    displayQuestion();
-});
+// ============ RENDERING END SCREEN =============
 
 function gameOver(){
-    timer.textContent = "";
-    question.textContent = "";
-    optionsField.textContent = "";
+    time.textContent = "";
+    optionsField.textContent = ""
     endScreen.classList.remove("hide")
     score.textContent = quizTime;
 }
 
+// ========= POPULATE USERS ARRAY WITH DATA FROM LOCAL STORAGE ======
 
 var users = []
 users = JSON.parse(localStorage.getItem("users"))
@@ -73,11 +53,12 @@ saveButton.addEventListener("click", function(){
 // ========= VIEW HIGH SCORES BUTTON ===========
 
 viewHighScores.addEventListener("click", function(){
-    endScreen.textContent = "";
+    startButton.textContent = ""
+    startButton.classList.remove("start-button")
     questionsField.textContent = "";
     // create users <ul> to add user <li> to
     var usersUl = document.createElement("ul");
-    endScreen.append(usersUl)
+    questionsField.append(usersUl)
     // create user <li>
     for (var i = 0; i < users.length; i++){
         var li = document.createElement("li");
@@ -85,26 +66,27 @@ viewHighScores.addEventListener("click", function(){
         usersUl.append(li);
         console.log(users[i].initials)
     }
-    
-    // li.id = users.length;
-
-    
-    
-    // console.log(users[1].initials)
 });
 
-// FIX DELAY BETWEEN 10 AND 9
+// ============= START QUIZ BUTTON ===============
+
+startButton.addEventListener ("click", function(){
+    setTimer();
+    displayQuestion();
+    startButton.textContent = ""
+    startButton.classList.remove("start-button")
+    // startButton.style.display = "none"
+});
+
+// =============== SET TIMER ===================
+
 function setTimer(){
     timer.textContent = quizTime;
-    
     var timerInterval = setInterval(function(){
-        
         timer.textContent = quizTime--;
-        
         if (quizTime < 0){
             quizTime = 0
         }
-
         if (quizTime <= 0 || !questions[questionObject]) {
             score = quizTime
             console.log(score)
@@ -114,10 +96,14 @@ function setTimer(){
     }, 1000)
 }
 
+// =============== DISPLAY QUESTION ===================
+
 function displayQuestion(){
     question.textContent = questions[questionObject].title;
     displayChoices();
 }
+
+// =============== DISPLAY CHOICES ==================
 
 function displayChoices(){
     // var options to store arrays of choices to refer to in the loop so that the code is clearer and easier to write
@@ -135,22 +121,9 @@ function displayChoices(){
     }
 }
 
-function updateQuestion(){
-    questionObject++
-    if (questions[questionObject]){
-        question.textContent = questions[questionObject].title;
-        optionsField.textContent = ""
-        displayChoices();
-    }
-    else {
-        gameOver()
-    } 
-}
+// ================== HANDLING CLICKED CHOICES =====================
 
 optionsField.addEventListener("click", function(event){
-    // score = quizTime
-    // console.log(score)
-    // clear question and options
     function clearQuestionAndOptions(){
         question.textContent = "";
         optionsField.textContent = "";
@@ -195,95 +168,20 @@ optionsField.addEventListener("click", function(event){
      
 })
 
+// ========== UPDATES QUESTION WHEN A CHOICE GETS CLICKED ==========
 
+function updateQuestion(){
+    questionObject++
+    if (questions[questionObject]){
+        question.textContent = questions[questionObject].title;
+        optionsField.textContent = ""
+        displayChoices();
+    }
+    else {
+        gameOver()
+    } 
+}
 
-// var gameover = document.createElement("h3");
-//     gameover.textContent = "You finished the quiz!"
-//     optionsField.appendChild(gameover);
-//     var scoreEl = document.createElement("h3");
-//     scoreEl.textContent = "Your score is " + quizTime
-//     optionsField.appendChild(scoreEl);
-//     var inputEl = document.createElement("input");
-//     inputEl.setAttribute("placeholder", "enter your initials here")
-//     inputEl.setAttribute("class", "initials")
-//     optionsField.appendChild(inputEl);
-//     // var userInitials = initialsEl.value
-//     // console.log(initialsEl)
-//     var save = document.createElement("div");
-//     save.textContent = "save";
-//     save.setAttribute("class", "save-btn")
-//     optionsField.appendChild(save);
-//     var saveBtn = document.querySelector("save-btn")
-//     saveBtn.addEventListener("click", function(){
-//         console.log("save is clicked")
-//     })
-    
+// CONSOLE.DIR TO BE SAVED FOR REFERENCE
 
-
-
-
-
-
-
-// when start quiz is cliked creates the choices again
-
-// function to input initials
-
-// function to create end UI
-
-// start at 100 pass the game in 50 sec, get remainder seconds as your score
-
-// subtract from timer when answer is wrong
-
-
-
-
-
-// need to go through questions array while 
-// var result = "";
-// do {
-//     questionObject++
-//     result = result + questionObject;
-//   } while (questionObject < 3);
-  
-//   console.log(result);
-//   // expected result: "12345"
-
-// OLD DISPLAY ANSWER
-// function displayAnswer(event){
-
-//     // console.log(event.target.value)    
-
-//     if(event.toElement.innerText === questions[questionObject].answer){
-//         question.textContent = "";
-//         optionsField.textContent = "";
-//         var correct = document.createElement("h3");
-//         correct.textContent = "Correct!"
-//         optionsField.appendChild(correct);
-//         var next = document.createElement("p");
-//         next.textContent = "next"
-//         optionsField.appendChild(next);
-//         next.addEventListener("click", function(){
-//             updateQuestion();
-//         });  
-//     }
-//     if (event.toElement.innerText !== questions[questionObject].answer){
-//         question.textContent = "";
-//         optionsField.textContent = "";
-//         var incorrect = document.createElement("h3");
-//         incorrect.textContent = "Incorrect:("
-//         optionsField.appendChild(incorrect);
-//         // displayNextButton();
-//         // subtract from timer when answer is wrong
-//     }
-    
-// }
-
-// THIS FOR LOOP HAS CONSOLE.DIR TO BE SAVED FOR REFERENCE
-
-// //  for(var i = 0; i < allChoices.length; i++){
-//     //     allChoices[i].addEventListener("click", function(event){
-//     //         console.dir(event.target.attributes.data.value)
-//     //     //    displayAnswer(event)
-//     //     })
-//     // }
+// console.dir(event.target.attributes.data.value)
